0c07011e484d62f5d8497c73ba79eb05f4fd9306
bahiep-droid <nguyenbahiep37na@gmail.com>
2025-10-07 16:55:46 +0700

    Add files via upload

---
+++ BAI2/Cau4-2.py
@@ -0,0 +1,6 @@
+print(" Sinh vien: Nguyen Ba Hiep")
+print(" MSV : 245752021610098")
+a = int(input("Enter a:"))
+b = int(input("Enter b:"))
+for k in range(a, b+1):
+    print("1 /", k, "=", 1 / k)
