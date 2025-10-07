776c4c410d49722986699fe3c475c05cf9e677ec
bahiep-droid <nguyenbahiep37na@gmail.com>
2025-10-07 16:30:17 +0700

    Add files via upload

---
+++ BAI2/Cau1-2.py
@@ -0,0 +1,9 @@
+print(" Sinh vien: Nguyen Ba Hiep")
+print(" MSV : 245752021610098")
+
+n1=int(input("enter n1 value"))
+n2=int(input("enter n2 value"))
+if n1>n2:
+       print("n1 is big")
+else:
+       print("n2 is big") 
