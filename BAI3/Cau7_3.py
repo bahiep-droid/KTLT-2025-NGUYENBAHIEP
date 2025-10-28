37d5538c79cb05277ea47729195b6032c207bd3f
bahiep-droid <nguyenbahiep37na@gmail.com>
2025-10-28 14:48:00 +0700

    Add files via upload

---
+++ BAI3/Cau7_3.py
@@ -0,0 +1,8 @@
+print(" Sinh vien: Nguyen Ba Hiep")
+print(" MSV : 245752021610098")
+def checkValue(n):
+    if n%2==0:
+     print('Day la mot so chan')
+    else:
+      print('Day la mot so le')
+checkValue(8)
