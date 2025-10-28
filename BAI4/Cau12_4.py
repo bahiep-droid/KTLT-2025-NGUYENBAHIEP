0dda83c5235676f338cad403fe0d97f9236f11dd
bahiep-droid <nguyenbahiep37na@gmail.com>
2025-10-28 16:25:41 +0700

    Add files via upload

---
+++ BAI4/Cau12_4.py
@@ -0,0 +1,7 @@
+print(" Sinh vien: Nguyen Ba Hiep")
+print(" MSV : 245752021610098")
+ds=input('Nhap chuoi: ').split()
+print(ds)
+if '123' in ds:
+    ds.remove('123')
+    print("Sau khi xóa '123' (lần đầu):", ds)
---
+++ BAI4/Cau14_4.py
@@ -0,0 +1,17 @@
+print(" Sinh vien: Nguyen Ba Hiep")
+print(" MSV : 245752021610098")
+
+ds = ['banana', 'apple', 'cherry', 'date', 'fig']
+
+print("Danh sách ban đầu:", ds)
+
+ds.sort()
+print("Sau khi sắp xếp (tăng dần):", ds)
+
+ds.sort(reverse=True)
+print("Sau khi sắp xếp (giảm dần):", ds)
+
+so = [5, 2, 8, 1, 9]
+print("\nDanh sách số:", so)
+so.sort()
+print("Sắp xếp số tăng dần:", so)
